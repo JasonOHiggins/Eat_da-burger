7cadb3e79f16d5240760d7d5088a97b84bf4aac9
JasonOHiggins <32472287+JasonOHiggins@users.noreply.github.com>
2018-05-22 17:00:09 -0500

    Heroku
    added Heroku JawsDB code to connection.js file

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,13 @@
 var mysql = require('mysql');
 
 // Create the MySQL connection object
-// var connection;
+var connection;
 
-// if (process.env.JAWSDB_URL) {
-// 	// DB is JawsDB on Heroku
-// 	connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-// 	// DB is local on localhost
+if (process.env.JAWSDB_URL) {
+	// DB is JawsDB on Heroku
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	// DB is local on localhost
 	var connection = mysql.createConnection({
 		port: 3306,
 		host: 'localhost',
@@ -16,7 +16,7 @@ var mysql = require('mysql');
 		password: 'Chicag@1',
 		database: 'burgers_db'
 	});
-
+};
 
 // Make the connection to MySQL
 connection.connect(function(err) {
